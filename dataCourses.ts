199e2764661526d843fe4d9ac54b3a65d5f8b9ab
coldblade2000 <diegogranada7@gmail.com>
2021-04-12 20:56:46 -0500

    Cree Course y dataCourses.ts, modifique index.html

---
+++ dataCourses.ts
@@ -0,0 +1,8 @@
+import {Course} from "./Course";
+
+export const dataCourses = [
+    new Course('Desarrollo SW', 3, 'Cesar Forero'),
+    new Course('DALGO', 3, 'Duitama'),
+    new Course('Genoma Humano', 2, 'Helen'),
+    new Course('Sistrans', 3, 'Claudia :('),
+]
